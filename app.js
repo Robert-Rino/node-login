293ff58d73b0e79e7c1e36ec7c780158b12363a8
張軒銘 <RobertRino@Rino-MacBook-Pro.local>
2016-12-28 18:53:58 +0800

    create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+let express = require('express')
+let bodyParser = require('body-parser');
+let app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/v1/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.post('/api/v1/register', function (req, res) {
+  console.log(req.body);
+  res.sendStatus(200)
+})
+
+app.listen(3000)
