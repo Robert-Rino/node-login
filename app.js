da80dcc2eca0e3248a0e45036bafc8a19c03a9fd
Robert-Rino <a901002666@gmail.com>
2016-12-30 11:26:27 +0800

    save register data to db

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict'
 let express = require('express');
-let bodyParser = require('body-parser');
+let bodyParser = require('body-parser')
 let jsonfile = require('jsonfile')
+let mongoose = require('mongoose')
 
-// let fileIo = require('./services/file_io');
 let app = express();
 
+let schema = new mongoose.Schema({ username: 'string', email: 'string' });
+let Account = mongoose.model('Account', schema);
+
+mongoose.connect('mongodb://localhost/');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,11 +18,11 @@ app.get('/api/v1/', function (req, res) {
 })
 
 app.post('/api/v1/register', function (req, res) {
-  console.log(req.body);
-  let file = 'data.json'
-  let obj = { [req.body.username]: req.body.email}
-  jsonfile.writeFile(file, obj, function (err) {
-  console.error(err)
+  console.log(req.body)
+  let account = new Account({ username: req.body.username, email:req.body.email });
+  account.save(function (err) {
+    if (err) return handleError(err);
+    // saved!
   })
   res.sendStatus(200)
 })
